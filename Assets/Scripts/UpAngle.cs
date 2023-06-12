49481ca0fd9eab8109a76d041e3c8d3d894bc2c7
JEWE7ER <crysisagent@mail.ru>
2023-06-12 20:51:13 +0500

    обновлённый механизм поднимания и опускания

--- Assets/Scripts/UpAngle.cs
+++ Assets/Scripts/UpAngle.cs
@@ -2,49 +2,44 @@ using UnityEngine;
 
 public class UpAngle : MonoBehaviour
 {
-    protected GameObject CameraLink;
-    Vector3 targetPosition;
+    private GameObject CameraLink;
+    private Vector3 targetPosition;
     private ChangeAngleRoom ChangeAngleRoomLink;
-    protected int currentAngle;
-    protected int oldAngle;
+    private int currentAngle;
+    private int oldAngle;
+    private bool moveAngle = false;
+    private float speed;
+
     public int numAngle;
     public float minY = 2.8f;
     public float maxY = 10.0f;
-    protected bool moveAngle = false;
-    protected float speed;
 
-    // Start is called before the first frame update
     void Start()
     {
         CameraLink = GameObject.Find("Main Camera");
         ChangeAngleRoomLink = CameraLink.GetComponent<ChangeAngleRoom>();
         currentAngle = ChangeAngleRoomLink.currentAngle;
-        //oldAngle = currentAngle;
-        //targetPosition = new Vector3(transform.position.x, maxY, transform.position.z);
         Transform();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        currentAngle = ChangeAngleRoomLink.currentAngle;
-        speed = ChangeAngleRoomLink.pSpeed + 0.5f;
-        if (currentAngle != oldAngle)
+        if (ChangeAngleRoomLink.isLerp)
         {
-            UpOrDown();
-        }
-        if (moveAngle)
-        {
-            transform.position = Vector3.Lerp(transform.position, targetPosition, speed * Time.deltaTime);
+            currentAngle = ChangeAngleRoomLink.currentAngle;
+            speed = ChangeAngleRoomLink.pSpeed + 0.5f;
+            if (currentAngle != oldAngle)
+                UpOrDown();
+            if (moveAngle)
+                transform.position = Vector3.Lerp(transform.position, targetPosition, speed * Time.deltaTime);
         }
     }
+
     void Transform()
     {
         UpOrDown();
         if (moveAngle)
-        {
             transform.position = targetPosition;
-        }
     }
 
     void UpOrDown()
--- Assets/Scripts/UpWall.cs
+++ Assets/Scripts/UpWall.cs
@@ -2,44 +2,36 @@ using UnityEngine;
 
 public class UpWall : MonoBehaviour
 {
-    protected GameObject CameraLink;
-    Vector3 targetPosition;
+    private GameObject CameraLink;
+    private Vector3 targetPosition;
     private ChangeAngleRoom ChangeAngleRoomLink;
-    protected int currentAngle;
-    protected int oldAngle;
+    private int currentAngle;
+    private int oldAngle;
+    private bool moveWall = false;
+    private float speed;
+
     public int numWall;
     public float minY = 2.8f;
     public float maxY = 10.0f;
-    protected bool moveWall = false;
-    protected float speed;
 
-
-    // Start is called before the first frame update
     void Start()
     {
         CameraLink = GameObject.Find("Main Camera");
         ChangeAngleRoomLink = CameraLink.GetComponent<ChangeAngleRoom>();
         currentAngle = ChangeAngleRoomLink.currentAngle;
-        //oldAngle = currentAngle;
-        //targetPosition = new Vector3(transform.position.x, maxY, transform.position.z);
         Transform();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        if (ChangeAngleRoomLink.cameraMove)
+        if (ChangeAngleRoomLink.isLerp)
         {
             currentAngle = ChangeAngleRoomLink.currentAngle;
             speed = ChangeAngleRoomLink.pSpeed + 0.5f;
             if (currentAngle != oldAngle)
-            {
                 UpOrDown();
-            }
             if (moveWall)
-            {
                 transform.position = Vector3.Lerp(transform.position, targetPosition, speed * Time.deltaTime);
-            }
         }
     }
 
@@ -55,12 +47,14 @@ public class UpWall : MonoBehaviour
 
     void UpOrDown()
     {
-        if (((numWall - currentAngle > 1) || ((numWall - currentAngle < 0) && (numWall - currentAngle > -3))) && transform.position.y > maxY - 2)
+        if (((numWall - currentAngle > 1) || ((numWall - currentAngle < 0) && (numWall - currentAngle > -3))) && 
+            transform.position.y > maxY - 2)
         {
             targetPosition = new Vector3(transform.position.x, minY, transform.position.z);
             moveWall = true;
         }
-        else if (((numWall - currentAngle == 1) || (numWall - currentAngle == 0) || (numWall - currentAngle == -3)) && transform.position.y < minY + 2)
+        else if (((numWall - currentAngle == 1) || (numWall - currentAngle == 0) || (numWall - currentAngle == -3)) && 
+            transform.position.y < minY + 2)
         {
             targetPosition = new Vector3(transform.position.x, maxY, transform.position.z);
             moveWall = true;
