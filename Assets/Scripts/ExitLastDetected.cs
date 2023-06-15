db1d5997a8c3c34b11b8ae82bb882ce0dfc2018d
JEWE7ER <crysisagent@mail.ru>
2023-06-16 00:49:58 +0500

    Формируем инфу для перехода

--- Assets/Scripts/ExitLastDetected.cs
+++ Assets/Scripts/ExitLastDetected.cs
@@ -3,36 +3,32 @@ using static TempValueCamera;
 
 public class ExitLastDetected : MonoBehaviour
 {
-    private GameObject CameraLink;
-    private ChangeAngleRoom ChangeAngleRoomLink;
-    private int currentAngle = 0;
-    private GameObject ScenesLink;
-    private Scenes scene;
-    private GameObject WallLink;
-    private UpWall wall;
+    private Camera cam;
     private bool onSpriteDown = false;
 
     private void OnMouseDown()
     {
-        WallLink = GameObject.Find(transform.parent.parent.name);
-        wall = WallLink.GetComponent<UpWall>();
-        CameraLink = GameObject.Find("Main Camera");
-        ChangeAngleRoomLink = CameraLink.GetComponent<ChangeAngleRoom>();
-        currentAngle = ChangeAngleRoomLink.currentAngle;
-        if (!ChangeAngleRoomLink.isLerp)
+        cam = Camera.main;
+        if (!cam.GetComponent<RotateRoom>().cameraMove)
         {
-            TempValueCamera.SetValue(CameraLink.transform.position, CameraLink.transform.eulerAngles,
-                currentAngle, onSpriteDown, wall.numWall);
-            ScenesLink = GameObject.Find("RoomChanger");
-            scene = ScenesLink.GetComponent<Scenes>();
-            scene.PrevRoom();
-            EngineSwipe.SwipeEvent -= ChangeAngleRoomLink.OnSwipe;
+            SetValue();
+            GameObject.Find("RoomChanger").GetComponent<Scenes>().PrevRoom();
+            EngineSwipe.SwipeEvent -= cam.GetComponent<RotateRoom>().OnSwipe;
         }
     }
+
+    private void SetValue()
+    {
+        TempValueCamera.CameraPosition = cam.transform.position;
+        TempValueCamera.CameraRotate = cam.transform.eulerAngles;
+        TempValueCamera.CurrentAngle = cam.GetComponent<RotateRoom>().currentAngle;
+        TempValueCamera.OnSpriteDown = onSpriteDown;
+        TempValueCamera.NumWall = GameObject.Find(transform.parent.parent.name).GetComponent<UpWall>().numWall;
+    }
+
     public void OnSpriteDown()
     {
         onSpriteDown = true;
         OnMouseDown();
-        onSpriteDown = false;
     }
 }
