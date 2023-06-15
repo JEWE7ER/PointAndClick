591e80076a0830752001efe1d7689157f794c54c
JEWE7ER <crysisagent@mail.ru>
2023-06-16 00:50:38 +0500

    данные с прошлой комнаты

--- Assets/Scripts/Temp Value Camera.cs
+++ Assets/Scripts/Temp Value Camera.cs
@@ -3,42 +3,9 @@ using UnityEngine;
 
 public static class TempValueCamera
 {
-    private static Vector3 _cameraPosition;
-    private static Vector3 _cameraRotate;
-    private static int _currentAngle;
-    private static bool _onSpriteDown;
-    private static int _numWall;
-
-    public static void SetValue(Vector3 cameraPosition, Vector3 cameraRotate, 
-        int currentAngle, bool onSpriteDown, int numWall)
-    {
-        _cameraPosition = cameraPosition;
-        _cameraRotate = cameraRotate;
-        _currentAngle = currentAngle;
-        _onSpriteDown = onSpriteDown;
-        _numWall = numWall;
-    }
-
-    public static int GetAngle()
-    {
-        return _currentAngle;
-    }
-
-    public static int GetNumWall()
-    {
-        return _numWall;
-    }
-
-    public static Vector3 GetCamPos()
-    {
-        return _cameraPosition;
-    }
-    public static Vector3 GetCamRot()
-    {
-        return _cameraRotate;
-    }
-    public static bool GetSpriteDown()
-    {
-        return _onSpriteDown;
-    }
+    internal static Vector3 CameraPosition { get; set; }
+    internal static Vector3 CameraRotate { get; set; }
+    internal static int CurrentAngle { get; set; } = 0;
+    internal static bool OnSpriteDown { get; set; }
+    internal static int NumWall { get; set; }
 }
