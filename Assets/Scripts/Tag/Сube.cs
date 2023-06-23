987275c4ad9d5e9c7410af495b33fe0fbf276267
JEWE7ER <crysisagent@mail.ru>
2023-06-23 16:25:17 +0500

    commit

--- Assets/Scripts/Tag/Сube.cs
+++ Assets/Scripts/Tag/Сube.cs
@@ -12,36 +12,32 @@ public class Cube : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-       
-
         col = GetComponent<BoxCollider>();
     }
     
 
-    void OnMouseDown()
+    private void OnMouseDown()
     {
-        
-        
         if (!manager.isWin)
         {
-            //col.enabled = false;
+            col.enabled = false;
             RaycastHit hit;
             
             if (!Physics.Linecast(transform.position, transform.position + transform.right,  out hit))
             {
-                transform.position = new Vector3(transform.position.x + (float)0.75, transform.position.y, transform.position.z);
+                transform.position = new Vector3(transform.position.x + 0.3f, transform.position.y, transform.position.z);
             }
             else if (!Physics.Linecast(transform.position, transform.position + -transform.right, out hit))
             {
-                transform.position = new Vector3(transform.position.x - (float)0.75, transform.position.y, transform.position.z);
+                transform.position = new Vector3(transform.position.x - 0.3f, transform.position.y, transform.position.z);
             }
             else if (!Physics.Linecast(transform.position, transform.position + transform.up, out hit))
             {
-                transform.position = new Vector3(transform.position.x, transform.position.y + (float)0.75, transform.position.z );
+                transform.position = new Vector3(transform.position.x, transform.position.y + 0.3f, transform.position.z );
             }
             else if (!Physics.Linecast(transform.position, transform.position + -transform.up, out hit))
             {
-                transform.position = new Vector3(transform.position.x, transform.position.y - (float)0.75, transform.position.z );
+                transform.position = new Vector3(transform.position.x, transform.position.y - 0.3f, transform.position.z );
             }
             col.enabled = true;
 
