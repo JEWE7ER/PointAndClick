cb884f31842f393cf9a05bbea03d3a11b7c0b0db
JEWE7ER <crysisagent@mail.ru>
2023-06-30 04:20:07 +0500

    commit

--- Assets/Scripts/Chest/OpenChest.cs
+++ Assets/Scripts/Chest/OpenChest.cs
@@ -1,25 +1,32 @@
-using System.Collections;
-using System.Collections.Generic;
+using System.Threading.Tasks;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
 public class OpenChest : MonoBehaviour
 {
     private Animator animator;
-    public key key;
+    private GameObject key;
+    private Animator keyAnimation;
+
     public int itemID;
     void Start()
     {
         animator = transform.parent.GetComponent<Animator>();
     }
 
-    private void OnMouseDown()
+    private async void OnMouseDown()
     {
         if (EventSystem.current.currentSelectedGameObject != null) {
             InventorySlot slot = SelectedSlot.Get().GetComponent<InventorySlot>();
             if (slot.ItemID == itemID)
             {
-                key.OpenKey();
+                key = Instantiate(slot.targetObject);
+                key.transform.parent = GameObject.FindGameObjectWithTag("Floor").transform.parent;
+                keyAnimation = key.transform.GetComponent<Animator>();
+                keyAnimation.enabled = true;
+                while (key.GetComponent<Key>().isEnd == false)
+                    await Task.Yield();
+                //Destroy(key);
                 Inventory.instance.DeleteSlot(slot);
                 animator.enabled = true;
 
--- Assets/Scripts/Chest/key.cs
+++ Assets/Scripts/Chest/key.cs
@@ -2,18 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class key : MonoBehaviour
+public class Key : MonoBehaviour
 {
-    private Animator animator;
-    // Start is called before the first frame update
-    void Start()
-    {
-        animator = transform.GetComponent<Animator>();
-    }
+    internal bool isEnd = false;
 
-    // Update is called once per frame
-    public void OpenKey()
+    public void EndAnimation()
     {
-        animator.enabled = true;
+        isEnd = true;
     }
 }
