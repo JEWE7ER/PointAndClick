1dc7a6d18645032d8df03798f2c2c1eb31a93d78
jenek594 <108339867+jenek594@users.noreply.github.com>
2023-07-04 04:55:38 +0500

    comm

---
+++ Assets/see.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class see : MonoBehaviour
+{
+    public KeyCode Left; 
+    public KeyCode Right;
+    private int i = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(Right))
+        {
+            gameObject.SetActive(true);
+            i += 1;
+        }
+        else if (Input.GetKey(Left))
+        {     
+            gameObject.SetActive(true);
+            i += 1;
+        }
+    }
+}
